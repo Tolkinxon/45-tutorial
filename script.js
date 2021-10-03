701e114429c47d2978a7609c9aadac6f15437044
Tolkinxon <krosstony639@gmail.com>
2021-10-03 12:07:42 +0500

    new commit

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
+/*
 function sum(a,b){
-    
+
     console.log(arguments);
 }
 
 sum(4,6,8);
-
-
+*/
 // arguments after run {'0':4, '1':6, '2':8} 
+
+
+
+
+
+
+
+//Method 1 of calculating the amount
+
+function sum(){
+    value = 0;
+    for(let key of arguments)
+        value += key;
+
+        return value;
+
+
+}
+
+console.log(sum(1,2,3,4,5)); 
+// sum after run 15
+
