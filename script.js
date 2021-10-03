762e8d9061c94a7216b550fb5b41eb07d1d95a0a
Tolkinxon <krosstony639@gmail.com>
2021-10-03 13:34:22 +0500

    new

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-/*
-function sum(){
+/*function sum(){
 
     console.log(arguments);
 }
 
-sum(4,6,8);
-*/
+sum(4,6,8);*/
 // arguments after run {'0':4, '1':6, '2':8} 
+
+
+
+
+
+
+// 3 point parameters
+/*function cars(...c){
+
+    console.log(c);
+
+}  
+cars(1,2,3,4,5,6,7,8,9);*/ 
+//there  will be an array after the run -> [1,2,3,4,5,6,7,8,9]      
 
 
 
@@ -15,9 +27,10 @@ sum(4,6,8);
 
 
 //Method 1 of calculating the amount
+/*function sum(){
 
-function sum(){
     value = 0;
+
     for(let key of arguments)
         value += key;
 
@@ -26,6 +39,6 @@ function sum(){
 
 }
 
-console.log(sum(1,2,3,4,5)); 
+console.log(sum(1,2,3,4,5)); */
 // sum after run 15
 
